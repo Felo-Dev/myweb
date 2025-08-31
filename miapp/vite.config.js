b0f50552992640ce8eab2ef869a2138f82cf7cf8
Felo-Dev <brandoncortes81@gmail.com>
2025-08-31 10:03:10 -0500

    Fix deploy: install react-icons, set base and deploy dir

--- miapp/vite.config.js
+++ miapp/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/myweb/", // <-- cambia si tu repo se llama distinto
   plugins: [react()],
-})
+});
