022d1466630201fa618874bb701206ef6a16fe7e
Brando felipe cortes <72344019+Felo-Dev@users.noreply.github.com>
2026-03-21 23:02:51 -0500

    Updates

--- miapp/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/myweb/", // <-- cambia si tu repo se llama distinto
-  plugins: [react()],
-});
